13862d58c667a0b645ba080ae1d7ff426134e494
forbrad <54485124+BrandonForbrad@users.noreply.github.com>
2022-09-30 12:15:47 -0400

    Update bundles.lua

--- src/modules/public/data/bundles.lua
+++ src/modules/public/data/bundles.lua
@@ -2,86 +2,86 @@ return {
     {
         Id = 182,
         Name = "Blue collar cat",
-        Price = 1
+        Price = 5
     },
     {
         Id = 131,
         Name = "Furious Finn",
-        Price = 1
+        Price = 6
     },
     {
         Id = 219,
         Name = "The Crook",
-        Price = 1
+        Price = 6 
     },
     {
         Id = 284,
         Name = "Penguin",
-        Price = 1
+        Price = 6
     },
     {
         Id = 215,
         Name = "Bombo",
-        Price = 1
+        Price = 8
     },
     {
         Id = 151,
         Name = "Golden Suit of Bling Squared",
-        Price = 1
+        Price = 12
     },
     {
         Id = 57,
         Name = "Ten Million Robux Man",
-        Price = 1
+        Price = 12
     },
     {
         Id = 11,
         Name = "White Collar Dog",
-        Price = 1
+        Price = 12
     },
     {
         Id = 182,
         Name = "Blue Collar Cat",
-        Price = 1
+        Price = 8
     },
     {
         Id = 248,
         Name = "Captain Crimson Jack",
-        Price = 1
+        Price = 8
     },
     {
         Id = 186,
         Name = "Team Roblox Ski",
-        Price = 1
+        Price = 9
     },
     {
         Id = 592,
         Name = "Davy Bazooka",
-        Price = 1
+        Price = 5
     },
     {
         Id = 795,
         Name = "KSI",
-        Price = 1
+        Price = 5
     },
     {
         Id = 229,
         Name = "Redcliff Elite Commander",
-        Price = 1
+        Price = 6
     },
     {
         Id = 276,
         Name = "Agent 53",
-        Price = 1
+        Price = 8
     },
     {
         Id = 874,
         Name = "Bubba Doll",
-        Price = 1
+        Price = 10
     },
     {
         Id = 162,
         Name = "Circuit Breaker",
-        Price = 1
+        Price = 10
     }
 }
